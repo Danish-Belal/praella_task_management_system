f74f8f2d601f6216e071decd01c6e54a2d59dc95
Danish-Belal <danishexplore019@gmail.com>
2025-06-18 17:36:15 +0530

    added project route to app

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 
 const authRoutes = require('./routes/authROutes');
+const projectRoutes = require('./routes/projectRoutes');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes)
 
 module.exports = app;
