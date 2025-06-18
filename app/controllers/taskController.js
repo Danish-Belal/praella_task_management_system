23167c5ec084f72d0a3985a3d64e18c5fc70f74b
Danish-Belal <danishexplore019@gmail.com>
2025-06-18 20:56:01 +0530

    Task controller

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -1,4 +1,4 @@
-const {createTask} = require('../models/taskModel')
+const {createTask,getTasksByProject,updateTask,deleteTask} = require('../models/taskModel')
 const {verifyProjectOwnership} = require('../utils/ownership')
 
 // Create Task
@@ -27,6 +27,84 @@ const handleCreateTask = async (req, res) => {
   }
 };
 
-module.exports={
-    handleCreateTask
-}
+// get all tasks for a project
+const handleGetTasks = async (req, res) => {
+  
+    const  projectId  = req.params.pId;
+    const  ownerId = req.user.id;
+
+    try {
+            await verifyProjectOwnership(projectId, ownerId);
+    }catch (err) {
+        return res.status(403).json({ success: false, message: err.message });
+    }
+
+    try {
+    const tasks = await getTasksByProject(projectId);
+    return res.status(200).json({ success: true, tasks });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// Update Task
+const handleUpdateTask = async (req, res) => {
+  const { id: taskId } = req.params;
+  const project_id = req.params.pId
+  const updates = req.body;
+  const ownerId = req.user.id;
+
+  
+
+  try {
+    await verifyProjectOwnership(project_id, ownerId);
+  } catch (err) {
+    return res.status(403).json({ success: false, message: err.message });
+  }
+
+  try {
+    const updatedTask = await updateTask(taskId, updates);
+    if (!updatedTask) {
+      return res.status(404).json({ success: false, message: 'Task not found' });
+    }
+
+    return res.status(200).json({ success: true, task: updatedTask });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// Delete Task
+const handleDeleteTask = async (req, res) => {
+  const { id: taskId } = req.params;
+   const project_id = req.params.pId
+  const ownerId = req.user.id;
+
+  if (!project_id) {
+    return res.status(400).json({ success: false, message: 'project_id is required' });
+  }
+
+  try {
+    await verifyProjectOwnership(project_id, ownerId);
+  } catch (err) {
+    return res.status(403).json({ success: false, message: err.message });
+  }
+
+  try {
+    const deleted = await deleteTask(taskId);
+    if (!deleted) {
+      return res.status(404).json({ success: false, message: 'Task not found or already deleted' });
+    }
+
+    return res.status(200).json({ success: true, task: deleted });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = {
+  handleCreateTask,
+  handleGetTasks,
+  handleUpdateTask,
+  handleDeleteTask
+};
