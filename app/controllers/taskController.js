62dd15548cf1957536fc706f11887724bd1c8f0b
Danish-Belal <danishexplore019@gmail.com>
2025-06-18 20:15:25 +0530

    default priority

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -1,9 +1,9 @@
 const {createTask} = require('../models/taskModel')
-const{getProjectById} = require('../models/projectModel')
+const {verifyProjectOwnership} = require('../utils/ownership')
 
 // Create Task
 const handleCreateTask = async (req, res) => {
-  try {
+  
     const { title, description,  priority, deadline} = req.body;
 
     if (!title || !deadline) {
@@ -13,17 +13,14 @@ const handleCreateTask = async (req, res) => {
     const ownerId = req.user.id;
     // console.log(ownerId);
     
-
-    // Verify ownership for the project.
-    const project = await getProjectById(projectId);
-    // console.log("Project owner id", project );
+    try {
+            await verifyProjectOwnership(projectId, ownerId);
+        }catch (err) {
+            return res.status(403).json({ success: false, message: err.message });
+        }
     
-    if (!project || project.owner_id !== ownerId) {
-      return res.status(403).json({ success: false, message: 'Access denied: This is not your project or prjoect not found' });
-    }
-    
-
-    const task = await createTask(title, description,  priority, deadline, projectId);
+    try {
+    const task = await createTask(title, description, deadline, projectId,priority,);
     return res.status(201).json({ success: true, task });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
--- app/models/taskModel.js
+++ app/models/taskModel.js
@@ -2,11 +2,11 @@ const {connectionPool} = require('../../config/db')
 
 
 // create a task for a project.
-const createTask = async (title, description,  priority, deadline, projectId) => {
+const createTask = async (title, description, deadline, projectId, priority='medium') => {
   const result = await connectionPool.query(
-    `INSERT INTO tasks (title, description,  priority, deadline, project_id)
+    `INSERT INTO tasks (title, description,  deadline, project_id,priority)
      VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-    [title, description,  priority, deadline, projectId]
+    [title, description,  deadline, projectId, priority]
   );
   return result.rows[0];
 };
