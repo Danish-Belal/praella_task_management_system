741ec76a0e070c94866ca614ae80c62c48b2a66e
Danish-Belal <danishexplore019@gmail.com>
2025-06-18 23:28:06 +0530

    added comment route to main route

--- app/index.js
+++ app/index.js
@@ -5,10 +5,13 @@ const app = express();
 const authRoutes = require('./routes/authROutes');
 const projectRoutes = require('./routes/projectRoutes');
 const tasksRoutes = require('./routes/taskRoute');
+const commentRoutes = require('./routes/commentRoute');
+
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', tasksRoutes);
+app.use('/api/comments', commentRoutes);
 
 module.exports = app;
