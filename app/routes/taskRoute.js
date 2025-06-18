1462531c94a7d5789bd01a3ad5038df1b84bfc63
Danish-Belal <danishexplore019@gmail.com>
2025-06-18 20:56:43 +0530

    Task route

--- app/routes/taskRoute.js
+++ app/routes/taskRoute.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const {handleCreateTask} = require('../controllers/taskController');
+const {
+    handleCreateTask,
+     handleGetTasks,
+    handleUpdateTask,
+    handleDeleteTask
+
+} = require('../controllers/taskController');
 
 router.use(authMiddleware)
 router.post('/:pId/createTask',  handleCreateTask);
+router.get('/getAllTasks/:pId',  handleGetTasks);
+router.put('/:pId/updateTask/:id',  handleUpdateTask);
+router.delete('/:pId/deleteTask/:id',  handleDeleteTask);
 
 
 
