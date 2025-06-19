0355dd914c669c3709da33d1061cd3eb7956f635
Danish-Belal <danishexplore019@gmail.com>
2025-06-20 03:17:46 +0530

    debuged

--- frontend/src/components/tasks/taskCard.tsx
+++ frontend/src/components/tasks/taskCard.tsx
@@ -16,7 +16,7 @@ const priorityColors = {
 };
 
 const statusColors = {
-  todo: 'bg-gray-100 text-gray-800',
+  'to-do': 'bg-gray-100 text-gray-800',
   'in-progress': 'bg-blue-100 text-blue-800',
   done: 'bg-green-100 text-green-800',
 };
