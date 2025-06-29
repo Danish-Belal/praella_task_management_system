2bd40cb5b7fb37358f55ad95d5caf844b1071611
Danish-Belal <danishexplore019@gmail.com>
2025-06-29 17:03:35 +0530

    Checking ursl

--- frontend/utils/healper.ts
+++ frontend/utils/healper.ts
@@ -2,6 +2,11 @@ export const getBaseUrl = (): string => {
   const awsUrl = import.meta.env.VITE_AWS_API_URL;
   const renderUrl = import.meta.env.VITE_RENDER_API_URL;
 
+  console.log("AWS URL", awsUrl);
+  console.log("Render url",renderUrl);
+  
+  
+
   if (!awsUrl || !renderUrl) {
     throw new Error("Missing required environment variables");
   }
